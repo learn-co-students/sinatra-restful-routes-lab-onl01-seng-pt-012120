c7795580ba52e5f860cddb717f768e28bec979b9
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-24 02:04:42 +0000

    Done.

--- db/migrate/20200424015752_recipes.rb
+++ db/migrate/20200424015752_recipes.rb
@@ -1,9 +1,9 @@
 class Recipes < ActiveRecord::Migration
   def change
-    create_table :users do |t|
+    create_table :recipes do |t|
     t.string :name
     t.string :ingredients
-    t.integer :cook_time
+    t.string :cook_time
   end
   end
 end
