4b0a6f67df6529597fda062ce9f0967eda151a0e
James <jarogers095@gmail.com>
2020-04-26 17:25:58 -0700

    Added show and edit views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,20 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/recipes" do
-    @recipes = Recipes.all
+    @recipes = Recipe.all
     erb :index
   end
+
+  get "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+
+    erb :show
+  end
+
+  get "/recipes/:id/edit" do
+    @recipe = Recipe.find(params[:id])
+
+    erb :edit
+  end
+
 end
